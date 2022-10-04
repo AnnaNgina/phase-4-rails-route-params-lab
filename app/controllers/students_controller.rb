ca2b71dfb41746a298b9b654f2a8c2cbd7c5a767
annastacia <annastaciangina96@gmail.com>
2022-10-05 01:05:09 +0300

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,23 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    q_first_name = params[:first_name] 
+    q_last_name = params[:last_name] 
+
+   students = if q_first_name
+      Student.find_by(first_name: q_first_name)
+    elsif q_last_name
+      Student.find_by(last_name: q_last_name)
+    else
+      Student.all
+    end
+
     render json: students
+  end
+  
+  def show
+    student = Student.find(params[:id]);
+    render json: student
   end
 
 end
